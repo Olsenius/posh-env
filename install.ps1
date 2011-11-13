8b575ce99ba79e1e19cb2d293f220a32115153ab
thedersen <thedersen@gmail.com>
2011-11-13 18:15:12 +0100

    Moved Get-FileEncoding to utilities

--- install.ps1
+++ install.ps1
@@ -27,22 +27,6 @@ if(!(Test-Path $PROFILE)) {
     New-Item $PROFILE -Force -Type File -ErrorAction Stop -WhatIf:$WhatIf > $null
 }
 
-# Adapted from http://www.west-wind.com/Weblog/posts/197245.aspx
-function Get-FileEncoding($Path) {
-    $bytes = [byte[]](Get-Content $Path -Encoding byte -ReadCount 4 -TotalCount 4)
-
-    if(!$bytes) { return 'utf8' }
-
-    switch -regex ('{0:x2}{1:x2}{2:x2}{3:x2}' -f $bytes[0],$bytes[1],$bytes[2],$bytes[3]) {
-        '^efbbbf'   { return 'utf8' }
-        '^2b2f76'   { return 'utf7' }
-        '^fffe'     { return 'unicode' }
-        '^feff'     { return 'bigendianunicode' }
-        '^0000feff' { return 'utf32' }
-        default     { return 'ascii' }
-    }
-}
-
 $profileLine = ". '$installDir\profile.ps1'"
 
 Write-Host "Adding posh-env to profile..."
