5af2459f82c756db873475f01408b7d8640ab7d4
thedersen <thedersen@gmail.com>
2011-11-13 17:57:02 +0100

    Added Beyond Compare config for git

--- git-config.ps1
+++ git-config.ps1
@@ -1,2 +1,20 @@
+# User
 git config --global user.name thedersen
 git config --global user.email thedersen@gmail.com
+
+# Core
+git config --global --replace-all core.autocrlf false
+git config --global --replace-all core.ignorecase true
+#git config --global core.editor mate -w
+
+# Diff/Merge (Beyond Compare)
+$bcomp = Get-ProgramFilesDir | Join-Path -ChildPath "beyond compare 3/bcomp.exe"
+git config --global diff.tool bc3
+git config --global difftool.bc3.path $bcomp
+
+git config --global merge.tool bc3
+git config --global mergetool.bc3.path $bcomp
+git config --global mergetool.keepBackup false
+
+# Aliases
+git config --global alias.last "cat-file commit HEAD"
--- utilities.psm1
+++ utilities.psm1
@@ -2,3 +2,12 @@ function Test-Win64(){
     $Processor = Get-WmiObject Win32_Processor | where {$_.DeviceID -eq "CPU0"} | select AddressWidth
     return $Processor.AddressWidth -eq 64
 }
+
+function Get-ProgramFilesDir() {
+  if (Test-Win64 -eq $true) {
+    (Get-Item "Env:ProgramFiles(x86)").Value
+  }
+  else {
+    (Get-Item "Env:ProgramFiles").Value
+  }
+}
