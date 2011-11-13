160c08174b632a6e10b979cedc30e642ecc9be04
thedersen <thedersen@gmail.com>
2011-11-13 18:11:45 +0100

    Updates submodules as first step in installation

--- install.ps1
+++ install.ps1
@@ -1,5 +1,12 @@
 param([switch]$WhatIf = $false)
 
+Write-Host "Updating git submodules"
+git submodule init
+git submodule update
+
+Write-Host "Building git-tfs"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\git-tfs\GitTfs.sln /p:Configuration=Vs2010_Debug
+
 if($PSVersionTable.PSVersion.Major -lt 2) {
     Write-Warning "posh-git requires PowerShell 2.0 or better; you have version $($Host.Version)."
     return
@@ -14,13 +21,6 @@ $installDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 if(!(. (Join-Path $installDir "posh-git\CheckVersion.ps1"))) {
     return
 }
-
-Write-Host "Updating git submodules"
-git submodule init
-git submodule update
-
-Write-Host "Building git-tfs"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\git-tfs\GitTfs.sln /p:Configuration=Vs2010_Debug
 
 if(!(Test-Path $PROFILE)) {
     Write-Host "Creating PowerShell profile...`n$PROFILE"
