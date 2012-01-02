aa198dd338733cbdb1dc3b543215d4c213f20c29
Andreas Lien Olsen <andreas.olsen@gmail.com>
2012-01-02 15:45:20 +0100

    Update path.ps1

--- path.ps1
+++ path.ps1
@@ -5,7 +5,7 @@ function Update-RelativePath($childPath) {
 
 if(Test-Win64 -eq $true) {
     Update-RelativePath("curl\win64"
-    $env:Path += ";" +${env:ProgramFiles(x86)}\git\bin
+    $env:Path += ";" +${env:ProgramFiles(x86)} + "\git\bin"
 }
 else {
 	Update-RelativePath("curl\win32")
